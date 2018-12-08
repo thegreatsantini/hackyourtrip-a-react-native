4a21fa7f478709bbd9b6fef14dee59273340d629
SirJUST <theoperaking@yahoo.com>
2018-12-08 14:37:54 -0800

    initial lambda functions

---
+++ serverless/create-opportunity.js
@@ -0,0 +1,44 @@
+import uuid from "uuid";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import {success, failure} from "./libs/response-lib";
+
+export async function main(event, context, callback) {
+	// Request body is passed in as a JSON encoded string in 'event.body'
+	const data = JSON.parse(event.body);
+	
+	const params = {
+		TableName: "Opportunities",
+		// 'Item' contains the attributes of the item to be created
+		// - 'userId': user identities are federated through the
+		// Cognito Identity Pool, we will use the identity id
+		// as the user id of the authenticated user
+		// - 'opportunityId': a unique uuid
+		// - 'pitch': parsed from request body
+		// - 'currency': parsed from request body
+		// - 'attachment': parsed from request body
+		// - 'location': Restaurant location (id?)
+		// - 'fundingAmount': parsed from request body
+		// - 'timeFrame': parsed from request body
+		// - 'createdAt': current Unix timestamp
+		
+		Item: {
+			userId: event.requestContext.identity.cognitoIdentityId,
+			opportunityId: uuid.v4(),
+			pitch: data.pitch,
+			currency: data.currency,
+			attachment: data.attachment,
+			location: data.location,
+			fundingAmount: data.fundingAmount,
+			timeFrame: data.timeFrame,
+			createdAt: Date.now()
+		}
+	};
+	
+	try {
+		await dynamoDbLib.call("put", params);
+		callback(null, success(params.Item));
+	} catch (e) {
+		console.log(e);
+		callback(null, failure({status: false}));
+	}
+}
