d3a8ff3ed995c950061aeab4d30672a388a1f229
SirJUST <theoperaking@yahoo.com>
2018-12-08 22:06:14 -0800

    import amplify and provide serverless config data

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 import AppNavigator from './config/routes';
 
 import { Provider } from './contexts';
+import Amplify from "aws-amplify";
+import config from "./config";
 
 export default class App extends React.Component {
   async componentWillMount() {
@@ -11,7 +13,35 @@ export default class App extends React.Component {
       Roboto: require('native-base/Fonts/Roboto.ttf'),
       Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
     });
+    this.configure();
   }
+
+  async configure() {
+    await Amplify.configure({
+      Auth: {
+          mandatorySignIn: true,
+          region: config.cognito.REGION,
+          userPoolId: config.cognito.USER_POOL_ID,
+          identityPoolId: config.cognito.IDENTITY_POOL_ID,
+          userPoolWebClientId: config.cognito.APP_CLIENT_ID
+      },
+      Storage: {
+          region: config.s3.REGION,
+          bucket: config.s3.BUCKET,
+          identityPoolId: config.cognito.IDENTITY_POOL_ID
+      },
+      API: {
+          endpoints: [
+              {
+                  name: "dc-concierge",
+                  endpoint: config.apiGateway.URL,
+                  region: config.apiGateway.REGION
+              },
+          ]
+      }
+  });
+  }
+
   render() {
     return (
       <Provider>
