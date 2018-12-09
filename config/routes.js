57c8df848eb1c08019f7c722620e37e9888f71d0
David Fry <david.fry.tv@gmail.com>
2018-12-09 10:18:45 -0800

    Added tab bar icons

--- config/routes.js
+++ config/routes.js
@@ -1,96 +1,132 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons, Entypo, MaterialIcons } from '@expo/vector-icons';
 
 import {
-    createSwitchNavigator,
-    createStackNavigator,
-    createAppContainer,
-    createBottomTabNavigator,
-    createDrawerNavigator
-} from 'react-navigation'
+  createSwitchNavigator,
+  createStackNavigator,
+  createAppContainer,
+  createBottomTabNavigator,
+  createDrawerNavigator,
+} from 'react-navigation';
 
-import FirstScreen from "../screens/FirstScreen";
-import InvestScreen from "../screens/InvestScreen";
-import RestaurantScreen from "../screens/RestaurantScreen";
-import ProfileScreen from "../screens/ProfileScreen";
-import AuthLoadingScreen from "../screens/AuthloadingScreen";
-import LoginScreen from "../screens/LoginScreen";
-import CreateAccountScreen from "../screens/CreateAccountScreen";
-import SettingsScreen from "../screens/SettingsScreen";
-import CurrencyScreen from "../screens/CurrencyScreen";
-import CurrencyListScreen from "../screens/CurrencyListScreen";
+import FirstScreen from '../screens/FirstScreen';
+import InvestScreen from '../screens/InvestScreen';
+import RestaurantScreen from '../screens/RestaurantScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+import AuthLoadingScreen from '../screens/AuthloadingScreen';
+import LoginScreen from '../screens/LoginScreen';
+import CreateAccountScreen from '../screens/CreateAccountScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+import CurrencyScreen from '../screens/CurrencyScreen';
+import CurrencyListScreen from '../screens/CurrencyListScreen';
 
-import {Provider} from "../CurrencyContext"
-import Sandbox from "../Sandbox";
-
+import { Provider } from '../CurrencyContext';
+import Sandbox from '../Sandbox';
 
 const AuthStackNavigator = createStackNavigator({
-    FirstScreen: {screen: FirstScreen},
-    LoginScreen: {screen: LoginScreen},
-    CreateAccountScreen: {screen: CreateAccountScreen}
-
-})
+  FirstScreen: { screen: FirstScreen },
+  LoginScreen: { screen: LoginScreen },
+  CreateAccountScreen: { screen: CreateAccountScreen },
+});
 
 const RestaurantStack = createStackNavigator({
-    CheckinScreen: {screen: RestaurantScreen},
-})
+  CheckinScreen: { screen: RestaurantScreen },
+});
 
 const AppointmentListStack = createStackNavigator({
-    ClassList: {screen: InvestScreen},
-
-})
+  ClassList: { screen: InvestScreen },
+});
 
 const InvestStack = createStackNavigator({
-    InvestScreen: {screen: InvestScreen},
-    // ProfileScreen: {},
-})
+  InvestScreen: { screen: InvestScreen },
+  // ProfileScreen: {},
+});
 
-const CurrencyStack = createStackNavigator({
-    CurrencyScreen: {screen: CurrencyScreen},
+const CurrencyStack = createStackNavigator(
+  {
+    CurrencyScreen: { screen: CurrencyScreen },
     // CurrencyListScreen: {
     //     screen: CurrencyListScreen,
     //     navigationOptions: ({navigation}) => ({
     //         headerTitle: navigation.state.params.title
     //     })
     // }
-}, {
-    mode: 'modal'
-})
+  },
+  {
+    mode: 'modal',
+  },
+);
 
-const AppTabNavigator = createBottomTabNavigator({
-    Restaurant: {screen: RestaurantStack},
-    Invest: {screen: InvestStack},
-    Currency: {screen: CurrencyStack},
-    Profile: {screen: ProfileScreen},
-})
+const AppTabNavigator = createBottomTabNavigator(
+  {
+    Restaurant: {
+      screen: RestaurantStack,
+      navigationOptions: {
+        tabBarLable: 'Restaurant',
+        tabBarIcon: ({ tintColor }) => (
+          <Ionicons name="ios-restaurant" size={32} color={tintColor} />
+        ),
+      },
+    },
+    Invest: {
+      screen: InvestStack,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Entypo name="line-graph" size={32} color={tintColor} />
+        ),
+      },
+    },
+    Currency: {
+      screen: CurrencyStack,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <MaterialIcons name="attach-money" size={32} color={tintColor} />
+        ),
+      },
+    },
+    Profile: {
+      screen: ProfileScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Entypo name="user" size={32} color={tintColor} />
+        ),
+      },
+    },
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: '#04C66D',
+      inactiveTintColor: '#D9D9D9',
+    },
+  },
+);
 
 const AppStackNavigator = createStackNavigator({
-    AppTabNavigator: {
-        screen: AppTabNavigator,
-        navigationOptions: ({ navigation }) => ({
-            title: 'Discover Investment Concierge',
-            headerLeft: (
-                <TouchableOpacity onPress={() => navigation.toggleDrawer()}>
-                    <View style={{ paddingHorizontal: 10 }}>
-                        <Ionicons name="md-menu" size={32} />
-                    </View>
-                </TouchableOpacity>
-            ),
-        }),
-    },
-
-})
+  AppTabNavigator: {
+    screen: AppTabNavigator,
+    navigationOptions: ({ navigation }) => ({
+      title: 'Discover Investment Concierge',
+      headerLeft: (
+        <TouchableOpacity onPress={() => navigation.toggleDrawer()}>
+          <View style={{ paddingHorizontal: 10 }}>
+            <Ionicons name="md-menu" size={32} />
+          </View>
+        </TouchableOpacity>
+      ),
+    }),
+  },
+});
 
 const AppDrawerNavigator = createDrawerNavigator({
-    Home: AppTabNavigator,
-    Settings: { screen: SettingsScreen },
-})
+  Home: AppTabNavigator,
+  Settings: { screen: SettingsScreen },
+});
 
 const AppNavigator = createSwitchNavigator({
-    AuthLoading: AuthLoadingScreen,
-    Auth: AuthStackNavigator,
-    App: AppDrawerNavigator
-})
+  AuthLoading: AuthLoadingScreen,
+  Auth: AuthStackNavigator,
+  App: AppDrawerNavigator,
+});
 
-export default createAppContainer(AppNavigator)
+export default createAppContainer(AppNavigator);
