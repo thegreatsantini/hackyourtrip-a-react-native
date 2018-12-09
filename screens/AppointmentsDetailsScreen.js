06e709f4bd4bd601d60524bef6c84e121a3b94a9
thegreatsantini <lss13@my.fsu.edu>
2018-12-09 12:59:19 -0800

    added fund button

--- screens/AppointmentsDetailsScreen.js
+++ screens/AppointmentsDetailsScreen.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import { View, StyleSheet, Text} from 'react-native';
+import React, { Component } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
 import {
     Body,
     Button,
@@ -15,41 +15,40 @@ import {
     Title
 } from "native-base";
 
-class AppointmentsDetailsScreen extends Component
-{
-    render()
-    {
-        const {projectImage} = this.props.navigation.state.params
+class AppointmentsDetailsScreen extends Component {
+    render() {
+        const { projectImage } = this.props.navigation.state.params
         return (
             <Container>
                 <Content>
                     {/*<Header>*/}
-                        {/*<Left>*/}
-                            {/*/!*<Button transparent>*!/*/}
-                                {/*/!*<Icon type="FontAwesome" name='angle-left' />*!/*/}
-                            {/*/!*</Button>*!/*/}
-                        {/*</Left>*/}
-                        {/*<Body>*/}
-                        {/*<Title>Details</Title>*/}
-                        {/*</Body>*/}
-                        {/*<Right>*/}
-                            {/*/!*<Button transparent>*!/*/}
-                                {/*/!*<Icon type="FontAwesome" name='bars' />*!/*/}
-                            {/*/!*</Button>*!/*/}
-                        {/*</Right>*/}
+                    {/*<Left>*/}
+                    {/*/!*<Button transparent>*!/*/}
+                    {/*/!*<Icon type="FontAwesome" name='angle-left' />*!/*/}
+                    {/*/!*</Button>*!/*/}
+                    {/*</Left>*/}
+                    {/*<Body>*/}
+                    {/*<Title>Details</Title>*/}
+                    {/*</Body>*/}
+                    {/*<Right>*/}
+                    {/*/!*<Button transparent>*!/*/}
+                    {/*/!*<Icon type="FontAwesome" name='bars' />*!/*/}
+                    {/*/!*</Button>*!/*/}
+                    {/*</Right>*/}
                     {/*</Header>*/}
-                    <Card style={{flex: 0}}>
+                    <Card style={{ flex: 0 }}>
                         <CardItem>
                             <View style={styles.header}>
-                                <Thumbnail round large source={{uri: projectImage}} style={{height: 200, width: 200, flex: 1, marginLeft: 60, marginTop: 30}} />
-                                <Text style={{paddingTop: 10, marginLeft: 80}}><Icon type="FontAwesome" name="map-pin" />Currently in - Seattle</Text>
+                                <Thumbnail round large source={{ uri: projectImage }} style={{ height: 200, width: 200, flex: 1, marginLeft: 60, marginTop: 30 }} />
+                                <Text style={{ paddingTop: 10, marginLeft: 80 }}><Icon type="FontAwesome" name="map-pin" />Currently in - Seattle</Text>
                                 {/*<Left><Text>{`\nMichael Litchev`}</Text></Left>*/}
-                                <Title style={{paddingTop: 20, paddingBottom: 10}}>Buffer Pitch</Title>
+                                <Title style={{ paddingTop: 20, paddingBottom: 10 }}>Buffer Pitch</Title>
                                 <Text>{`Buffer is a social media scheduling platform that helps you schedule content to Facebook, Twitter, Linkedin and Pinterest`}</Text>
                                 <CardItem>
                                     <Left>
                                         <Text>Closing Date: 1/1/19</Text>
                                     </Left>
+
                                     <Right>
                                         <Text>Amount Need: $100,000</Text>
                                     </Right>
@@ -59,15 +58,23 @@ class AppointmentsDetailsScreen extends Component
                         </CardItem>
                         <CardItem>
                             <Left>
-                                <Button transparent textStyle={{color: '#87838B'}}>
+                                <Button transparent textStyle={{ color: '#87838B' }}>
                                     <Icon name="logo-github" />
-                                    <Text style={{paddingLeft: 5}}>GitHub</Text>
+                                    <Text style={{ paddingLeft: 5 }}>GitHub</Text>
                                 </Button>
                             </Left>
+                            <Body>
+                                <Button
+                                    style={{ padding: 5 }}
+                                    bordered
+                                >
+                                    <Text>Fund Project</Text>
+                                </Button>
+                            </Body>
                             <Right>
-                                <Button transparent textStyle={{color: '#87838B'}}>
+                                <Button transparent textStyle={{ color: '#87838B' }}>
                                     <Icon name="logo-linkedin" />
-                                    <Text style={{paddingLeft: 5}}>LinkedIn</Text>
+                                    <Text style={{ paddingLeft: 5 }}>LinkedIn</Text>
                                 </Button>
                             </Right>
                         </CardItem>
