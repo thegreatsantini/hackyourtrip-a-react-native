86fdd8e7783592fb2be41613f813c58d58705a40
thegreatsantini <lss13@my.fsu.edu>
2018-12-09 11:49:18 -0800

    created Spinner component to handle loading

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import  authenticateUser  from "./utils"
 
 
 import AppNavigator from './config/routes';
