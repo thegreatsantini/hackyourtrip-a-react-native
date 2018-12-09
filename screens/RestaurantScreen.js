d3d5547894b151717cec8c0b6802115f76982743
thegreatsantini <lss13@my.fsu.edu>
2018-12-08 22:16:17 -0800

    implented RestaurantCard compnonent in Screen container

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -1,37 +1,56 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
-
+import RestaurantCard from '../components/RestaurantCard'
 // Contexts
 import AppStateContext from '../contexts'
 
-class RestaurantScreen extends Component {
-    componentDidMount() {
-        console.log()
+const hardCoded = [
+    {
+        name: "Joe's crabshack", 
+        address: "133 new haven", 
+        city: 'toronto', 
+        mcc: 'steakhouse', 
+        website: 'google.com'
     }
-  render() {
-    return (
-        <AppStateContext.Consumer>
-            {
-                (value) => {
-                    return (
-                        <View style={styles.container}>
-                            <Text>Restaurant List Screen {value}</Text>
-                        </View>
-                    )
-                }
-            }
-        </AppStateContext.Consumer>
+]
 
-    );
-  }
+class RestaurantScreen extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            restaurants: [],
+            isLoading: true
+        }
+    }
+    renderRestaurants = async (token) => {
+        return ['sushi', 'BBQ', 'SteakHouse']
+    }
+    render() {
+        return (
+            // <AppStateContext.Consumer>
+            //     {
+            //         (value) => {
+            //             return (
+            //                 <View style={styles.container}>
+            //                     <Text>Restaurant List Screen {value}</Text>
+            //                 </View>
+            //             )
+            //         }
+            //     }
+            // </AppStateContext.Consumer>
+            <View style={styles.container}>
+                <RestaurantCard restaurants={hardCoded}  />
+            </View>
+
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        margin: 2
+    },
 });
 
 export default RestaurantScreen;
